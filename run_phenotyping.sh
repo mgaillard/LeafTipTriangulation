f95a30cbc48af5c81510038c5ab8bbb4e6a1190d
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2022-05-31 11:25:22 -0400

    Export the baseline histogram and scatter plot for the sorghum datasets

--- run_phenotyping.sh
+++ run_phenotyping.sh
@@ -4,18 +4,35 @@ LeafTipTriangulationCmd="./build/bin/Release/LeafTipTriangulation.exe"
 phenotype="tips"
 theta="0"
 
+copy_without_header_line() {
+    # NR>1 skips the first line of the file, which is the header
+    awk 'NR>1 {m=$2;for(i=2;i<=NF;i++)if($i>m)m=$i;print $1"\t"m}' $1 > $2
+}
+
+process_sorghum_dataset() {
+    local directory=$1
+    $LeafTipTriangulationCmd leaf_counting "Phenotyping/$directory" $phenotype $theta 0 0.0 "results/$directory/results.csv" "results/$directory/nb_annotations.csv"
+    # Find the maximum number of annotated leaves in any view of each plant
+    # This becomes the baseline for any single-view detection based method
+    copy_without_header_line "results/$directory/nb_annotations.csv" "results/$directory/results_baseline.csv"
+    # This is the result of the baseline
+    python3 Phenotyping/scripts/compare_to_ground_truth.py --command graphs --input "results/$directory/results_baseline.csv" --truth "Phenotyping/$directory/annotations/ground_truth.csv" --output "results/$directory"
+    # Rename the output files for the baseline
+    mv "results/$directory/histogram.png" "results/$directory/histogram_baseline.png"
+    mv "results/$directory/scatter.png" "results/$directory/scatter_baseline.png"
+    python3 Phenotyping/scripts/compare_to_ground_truth.py --command graphs --input "results/$directory/results.csv" --truth "Phenotyping/$directory/annotations/ground_truth.csv" --output "results/$directory"
+}
+
 mkdir -p results
 
 # Processing the sorghum 2018 dataset
 mkdir -p results/sorghum_2018
-$LeafTipTriangulationCmd leaf_counting Phenotyping/sorghum_2018 $phenotype $theta 0 0.0 results/sorghum_2018/results.csv results/sorghum_2018/nb_annotations.csv
-python3 Phenotyping/scripts/compare_to_ground_truth.py --command graphs --input results/sorghum_2018/results.csv --truth Phenotyping/sorghum_2018/annotations/ground_truth.csv --output results/sorghum_2018
+process_sorghum_dataset sorghum_2018
 
 # Processing the sorghum 2022 dataset
 mkdir -p results/sorghum_2022
 python3 Phenotyping/scripts/convert_cvat_to_csv.py --input Phenotyping/sorghum_2022/annotations/annotations.xml > Phenotyping/sorghum_2022/leaf_tips.csv
-$LeafTipTriangulationCmd leaf_counting Phenotyping/sorghum_2022 $phenotype $theta 0 0.0 results/sorghum_2022/results.csv results/sorghum_2022/nb_annotations.csv
-python3 Phenotyping/scripts/compare_to_ground_truth.py --command graphs --input results/sorghum_2022/results.csv --truth Phenotyping/sorghum_2022/annotations/ground_truth.csv --output results/sorghum_2022
+process_sorghum_dataset sorghum_2022
 
 # Merge the results of the two datasets and compare to observations
 cat results/sorghum_2018/results.csv results/sorghum_2022/results.csv > results/results.csv
@@ -41,8 +58,7 @@ compute_agreement_for_all_seeds() {
         $LeafTipTriangulationCmd leaf_counting Phenotyping/sorghum_2022 $phenotype $theta $s $probability $resultCsvFile $annotationCsvFile
         # Find the maximum number of annotated leaves in any view of each plant
         # This becomes the baseline for any single-view detection based method
-        # NR>1 skips the first line of the file, which is the header
-        awk 'NR>1 {m=$2;for(i=2;i<=NF;i++)if($i>m)m=$i;print $1"\t"m}' $annotationCsvFile > $resultBaseCsvFile
+        copy_without_header_line $annotationCsvFile $resultBaseCsvFile
         # Compare the number of leaves from the result file
         # This is the result of our method
         python3 Phenotyping/scripts/compare_to_ground_truth.py --command values --input $resultCsvFile --truth Phenotyping/sorghum_2022/annotations/ground_truth.csv --output results/sorghum_2022 >> $resultProbabilityFile
