9c00f892856d46fb41dc280730aaaa6688c48415
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2022-06-16 17:40:44 +0200

    Update RCAC jobs

--- jobs/compile.sh
+++ jobs/compile.sh
@@ -4,8 +4,10 @@
 # Launch from the root of the repository from the login node
 #-----------------------------------------------------------
 
+module load anaconda/2020.11-py38
 module load cmake/3.20.6
 module load gcc/10.2.0
+module load gnuplot
 
 # Launch the build
 mkdir -p build
@@ -26,3 +28,13 @@ cd datamash-1.7
 make
 make check
 cd ..
+
+# Install Make 4.3
+wget http://ftp.gnu.org/gnu/make/make-4.3.tar.gz
+tar xvf make-4.3.tar.gz
+rm make-4.3.tar.gz
+cd make-4.3
+./configure --prefix=/usr --without-guile --host=$LFS_TGT --build=$(build-aux/config.guess)
+make -j16
+cd ..
+./make-4.3/make --version
