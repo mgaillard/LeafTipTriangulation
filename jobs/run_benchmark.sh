c9bc6aa39cd5388c27ae797b90a6e65e1867a83b
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2022-06-17 15:28:25 +0200

    Add a RCAC job for threshold benchmark

---
+++ jobs/run_benchmark.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+#FILENAME: run_benchmark.sub
+#SBATCH --nodes=1
+#SBATCH --ntasks=128
+#SBATCH --time=0:10:00
+#SBATCH --job-name LeafTipTriangulation
+
+module load gcc/9.3.0
+module load gnuplot
+
+./build/bin/Release/LeafTipTriangulation.exe correspondence_threshold > plots/correspondence_threshold.dat
+gnuplot -e "filename='plots/correspondence_threshold.dat'" "plots/correspondence_threshold_1.pg" > results/correspondence_threshold_1.pdf
+gnuplot -e "filename='plots/correspondence_threshold.dat'" "plots/correspondence_threshold_2.pg" > results/correspondence_threshold_2.pdf
