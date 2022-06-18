0d65f6605af980bcd51d34a24b9bde086d2c4902
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2022-06-18 10:33:49 +0200

    Use different best values for each dataset

--- run_phenotyping.sh
+++ run_phenotyping.sh
@@ -2,7 +2,10 @@
 
 LeafTipTriangulationCmd="./build/bin/Release/LeafTipTriangulation.exe"
 phenotype="tips"
-theta="1500"
+# Best value of theta per dataset
+theta2018="1250"
+theta2022="2000"
+theta2022_synthetic="1000"
 
 copy_without_header_line() {
     # NR>1 skips the first line of the file, which is the header
@@ -11,6 +14,7 @@ copy_without_header_line() {
 
 process_sorghum_dataset() {
     local directory=$1
+    local theta=$2
     $LeafTipTriangulationCmd leaf_counting "Phenotyping/$directory" $phenotype $theta 0 0.0 "results/$directory/results.csv" "results/$directory/nb_annotations.csv"
     # Find the maximum number of annotated leaves in any view of each plant
     # This becomes the baseline for any single-view detection based method
@@ -27,16 +31,16 @@ mkdir -p results
 
 # Processing the sorghum 2018 dataset
 mkdir -p results/sorghum_2018
-process_sorghum_dataset sorghum_2018
+process_sorghum_dataset sorghum_2018 $theta2018
 
 # Processing the sorghum 2022 dataset
 mkdir -p results/sorghum_2022
 python3 Phenotyping/scripts/convert_cvat_to_csv.py --input Phenotyping/sorghum_2022/annotations/annotations.xml > Phenotyping/sorghum_2022/leaf_tips.csv
-process_sorghum_dataset sorghum_2022
+process_sorghum_dataset sorghum_2022 $theta2022
 
 # Processing the sorghum 2022 (synthetic) dataset
 mkdir -p results/sorghum_2022_synthetic
-process_sorghum_dataset sorghum_2022_synthetic
+process_sorghum_dataset sorghum_2022_synthetic $theta2022_synthetic
 
 # Merge the results of the two datasets and compare to observations
 cat results/sorghum_2018/results.csv results/sorghum_2022/results.csv > results/results.csv
