8bcb7586848b5155a529f33cd6ab0f4763462490
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2022-08-02 22:28:07 -0400

    Change tabs to spaces

--- LeafTipTriangulationLib/include/RayMatching.h
+++ LeafTipTriangulationLib/include/RayMatching.h
@@ -20,7 +20,7 @@ void removeSingleRays(std::vector<std::vector<std::pair<int, int>>>& setsOfRays)
  * \param setsOfRays A matching of rays
  */
 void removePointsFromSingleRays(std::vector<glm::dvec3>& points,
-	                            std::vector<std::vector<std::pair<int, int>>>& setsOfRays);
+                                std::vector<std::vector<std::pair<int, int>>>& setsOfRays);
 
 /**
  * \brief Sort each set of rays
@@ -40,11 +40,11 @@ void sortSetsOfRays(std::vector<std::vector<std::pair<int, int>>>& setsOfRays);
  * \return True if the similarities could be saved in the file
  */
 bool computeDistributionOfSimilarities(
-	const std::string& filename,
-	const std::vector<Camera>& cameras,
-	const std::vector<std::vector<glm::dvec2>>& points2d,
-	const std::vector<std::vector<Ray>>& rays,
-	const std::vector<std::vector<std::pair<int, int>>>& setsOfRays);
+    const std::string& filename,
+    const std::vector<Camera>& cameras,
+    const std::vector<std::vector<glm::dvec2>>& points2d,
+    const std::vector<std::vector<Ray>>& rays,
+    const std::vector<std::vector<std::pair<int, int>>>& setsOfRays);
 
 /**
  * \brief Find a matching of rays between multiple cameras and triangulate 3D points based on the matching
@@ -55,7 +55,7 @@ bool computeDistributionOfSimilarities(
  * \return Triangulated 3D points and the matching of rays that triangulates 2D points projected by cameras
  */
 std::tuple<std::vector<glm::dvec3>, std::vector<std::vector<std::pair<int, int>>>> matchRaysAndTriangulate(
-	const std::vector<Camera>& cameras,
-	const std::vector<std::vector<glm::dvec2>>& points2d,
-	const std::vector<std::vector<Ray>>& rays,
-	double thresholdNoPair = std::numeric_limits<double>::max());
+    const std::vector<Camera>& cameras,
+    const std::vector<std::vector<glm::dvec2>>& points2d,
+    const std::vector<std::vector<Ray>>& rays,
+    double thresholdNoPair = std::numeric_limits<double>::max());
